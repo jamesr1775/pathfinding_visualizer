08a822a8d109602a3236ba95bdd1c0cfd0575409
jamesr1775 <jamesr1775@yahoo.co.uk>
2020-09-07 00:51:03 +0100

    first commit

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,182 @@
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import { dijkstra } from "../algorithms/dijkstra";
+import { breadthFirstSearch } from "../algorithms/breadthFirstSearch";
+
+import { getNodesInShortestPathOrder } from "../algorithms/algorithmUtils";
+
+import "./PathfindingVisualizer.css";
+
+const NUM_GRID_ROWS = 30;
+const NUM_GRID_COLS = 50;
+
+const START_NODE_ROW = 14;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 14;
+const FINISH_NODE_COL = 19;
+
+export default class PathfindingVisualizer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grid: [],
+      mouseIsPressed: false,
+    };
+  }
+
+  componentDidMount() {
+    const grid = getInitialGrid();
+    this.setState({ grid });
+  }
+
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  handleMouseUp() {
+    this.setState({ mouseIsPressed: false });
+  }
+
+  animateAlgorithm(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-visited";
+      }, 10 * i);
+    }
+  }
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-shortest-path";
+      }, 50 * i);
+    }
+  }
+
+  visualizeDijkstra() {
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateAlgorithm(visitedNodesInOrder, nodesInShortestPathOrder);
+  }
+  visualizeBreadthFirstSearch() {
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = breadthFirstSearch(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateAlgorithm(visitedNodesInOrder, nodesInShortestPathOrder);
+  }
+
+  render() {
+    const { grid, mouseIsPressed } = this.state;
+
+    return (
+      <>
+        <button onClick={() => this.visualizeDijkstra()}>
+          Visualize Dijkstra's Algorithm
+        </button>
+        <button onClick={() => this.visualizeBreadthFirstSearch()}>
+          Visualize Breadth First Search Algorithm
+        </button>
+        <button onClick={() => getClearGrid(grid)}>Clear Board
+        </button>
+
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isFinish, isStart, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      mouseIsPressed={mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                      row={row}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < NUM_GRID_ROWS; row++) {
+    const currentRow = [];
+    const isWall = false;
+    for (let col = 0; col < NUM_GRID_COLS; col++) {
+      currentRow.push(createNode(col, row, isWall));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (col, row, isWall) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: isWall,
+    previousNode: null,
+  };
+};
+
+const getClearGrid = (grid) => {
+  for (let row = 0; row < NUM_GRID_ROWS; row++) {
+    const currentRow = [];
+    for (let col = 0; col < NUM_GRID_COLS; col++) {
+      const { isWall } = grid[row][col];
+      grid[row][col] = createNode(col, row, isWall);
+    }
+  }
+  return grid;
+};
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
---
+++ src/algorithms/breadthFirstSearch.js
@@ -0,0 +1,34 @@
+import * as utils from "./algorithmUtils";
+// Performs Breadth first search algorithm; returns *all* nodes in the order
+// in which they were visited. Also makes nodes point back to their
+// previous node, effectively allowing us to compute the shortest path
+// by backtracking from the finish node.
+export function breadthFirstSearch(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  startNode.distance = 0;
+  //const unvisitedNodes = getAllNodes(grid);
+  var queue = [startNode];
+  const nodeQueued = grid.map((row) => row.map((value) => false));
+
+  while (queue.length > 0) {
+    //sortNodesByDistance(unvisitedNodes);
+    const currentNode = queue.shift();
+    currentNode.isVisited = true;
+    utils.updateUnvisitedNeighbors(currentNode, grid);
+
+    if (currentNode.isWall) continue;
+
+    visitedNodesInOrder.push(currentNode);
+    let neighbouringNodes = utils.getUnvisitedNeighbors(currentNode, grid);
+    neighbouringNodes.forEach((neighbour) => {
+      let { col, row } = neighbour;
+      if (nodeQueued[row][col] === false) {
+        queue.push(neighbour);
+        nodeQueued[row][col] = true;
+      }
+    });
+
+    if (currentNode === finishNode) return visitedNodesInOrder;
+  }
+  return visitedNodesInOrder;
+}
